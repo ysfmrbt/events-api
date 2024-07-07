c10001e5a5de6bff85261d69ca04a5e0a2b8acc5
youssefmrabetsm <youssef.mrabet@seekmake.tech>
2024-07-07 10:07:43 +0100

    Initial Project

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+import { getPgVersion } from './neon'
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+const port = 3000
+console.log(`Server is running on port ${port}`)
+
+getPgVersion().then(() => {
+  console.log('Postgres is connected')
+}, (err) => {
+  console.error(err)
+})
+
+serve({
+  fetch: app.fetch,
+  port
+})
---
+++ src/neon.ts
@@ -0,0 +1,27 @@
+import postgres from 'postgres'
+import dotenv from 'dotenv'
+dotenv.config()
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env
+PGPASSWORD = decodeURIComponent(PGPASSWORD as string)
+
+console.info(`PGHOST: ${PGHOST}`)
+console.info(`PGDATABASE: ${PGDATABASE}`)
+console.info(`PGUSER: ${PGUSER}`)
+console.info(`PGPASSWORD: ${PGPASSWORD}`)
+
+const sql = postgres({
+    host: PGHOST,
+    database: PGDATABASE,
+    username: PGUSER,
+    password: PGPASSWORD,
+    port: 5432,
+    ssl: 'require',
+    connection: {
+        options: `project=${ENDPOINT_ID}`
+    }
+})
+
+export async function getPgVersion() {
+    const result = await sql`select version()`
+    console.log(result)
+}
